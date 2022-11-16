111012da360d8ef22e2a8b507a1424939d26f83f
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2022-11-15 22:32:46 -0800

    a

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('Pages.Home.index');
+        return view('Pages.dashboard.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', HomeController::class, 'index')->name('index');
+Route::get('/', [HomeController::class, "index"])->name('dashboard');
