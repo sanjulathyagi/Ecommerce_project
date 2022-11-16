80a4861b303100b3fd9feb138017ab8a01795097
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2022-11-15 23:42:55 -0800

    a

---
+++ resources/views/libraries/styles.blade.php
@@ -0,0 +1,9 @@
+@livewireStyles
+
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+    <!-- font awesome style -->
+    <link href="css/font-awesome.min.css" rel="stylesheet" />
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet" />
+    <!-- responsive style -->
+    <link href="css/responsive.css" rel="stylesheet" />
